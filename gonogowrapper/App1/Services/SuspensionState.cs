f31b3f8c965781226b6439995602ba357b2a03f5
Jeremiah Stillings <jstillings1@outlook.com>
2018-09-29 05:43:13 -0400

    missed dir
    sln included now

---
+++ gonogowrapper/App1/Services/SuspensionState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace App1.Services
+{
+    public class SuspensionState
+    {
+        public object Data { get; set; }
+
+        public DateTime SuspensionDate { get; set; }
+    }
+}
---
+++ gonogowrapper/App1/Views/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using Windows.UI.Xaml.Controls;
+
+namespace App1.Views
+{
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var appView = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView();
+            appView.Title = "No Go Go ADA Checker";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private void Forground_Color_ColorChanged(ColorPicker sender, ColorChangedEventArgs args)
+        {
+
+            ResultUpdate(GoNoGoCalcAA(sender, Background_Color), GoNoGoCalcAAA(Forground_Color, sender));
+        }
+    
+
+        private void Background_Color_ColorChanged(ColorPicker sender, ColorChangedEventArgs args)
+        {
+            ResultUpdate(GoNoGoCalcAA(sender, Background_Color),GoNoGoCalcAAA(Forground_Color, sender));
+        }
+        private string GoNoGoCalcAA(ColorPicker front, ColorPicker back)
+        { string result = "";
+
+            result = " this is a test for AA";
+            return result;
+
+        }
+        private string GoNoGoCalcAAA(ColorPicker front, ColorPicker back)
+        {
+            string result = "";
+
+            result = " this is a test for AAA";
+            return result;
+
+        }
+
+        private void ResultUpdate( string aa, string aaa)
+        {
+            AA_Result.Text = aa;
+            AAA_Result.Text = aaa;
+        }
+
+    }
+}
